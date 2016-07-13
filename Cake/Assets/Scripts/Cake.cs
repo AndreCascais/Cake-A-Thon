a9c751c23042ccd94e856cf8ecb00d4cb5deb82b
AndreCascais <a_cascais@hotmail.com>
2016-07-13 15:50:36 +0100

    Script Changes/Additions
    Tiling destroys out of cam backgrounds
    Player has 2 lives -> destroyed when reach 0

---
+++ Cake/Assets/Scripts/Cake.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cake : MonoBehaviour {
+
+	// Use this for initialization
+
+	[System.Serializable]
+	public class PlayerStats{
+		public int lives=2;
+	}
+	public PlayerStats playerStats = new PlayerStats();
+	public void TakeLive(){
+		playerStats.lives--;
+		Debug.Log (playerStats.lives);
+		if(playerStats.lives==0){
+			Debug.Log ("Era suposto rip");
+			GameMaster.KillCake(this);
+		}
+	}
+}
